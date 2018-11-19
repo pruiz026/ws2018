7c0afc260b430fcb19bda194e834a563ed2c6485
“Pablo <pablo191719@gmail.com>
2018-11-19 16:31:58 +0100

    lab05

--- Lab05/js/addQuestion.js
+++ Lab05/js/addQuestion.js
@@ -1,38 +1,53 @@
-$(document).ready(function(){
-$("#formularioa").submit(function(){
-	var eposta = $("#eposta").val().trim(eposta);				
-	var galdera = $("#galdera").val().trim(galdera).replace(/\s+/g, " ");
-	var erantzunZuzena = $("#erantzunZuzena").val();				
-	var erantzunOkerra1 = $("#erantzunOkerra1").val();
-	var erantzunOkerra2 = $("#erantzunOkerra2").val();				
-	var erantzunOkerra3 = $("#erantzunOkerra3").val();
-	var zailtasuna = $("#zailtasuna").val();				
-	var arloa = $("#arloa").val();
-	var irudia = $("#fitxategia").val();
-	
-	var erroreak = "";
-	if(eposta == "") erroreak += "(*) Egilearen eposta zehaztu gabe dago\n";
-	else {
-		var epostaExp = new RegExp("^[a-zA-Z]{3,}[0-9]{3}@ikasle\.ehu\.eus$");
-		if(!epostaExp.test(eposta)) erroreak += "(*) Eposta okerra\n";
-	}
-	
-	if(galdera == "") erroreak += "(*) Galderaren testua zehaztu gabe dago\n";
-	else if(galdera.length < 10) erroreak += "(*) Galderaren testua motzegia da, 10 ko luzera ez du gainditzen\n";
-	if(erantzunZuzena == "") erroreak += "(*) Erantzun zuzena zehaztu gabe dago\n";
-	if(erantzunOkerra1 == "") erroreak += "(*) Erantzun okerra1 zehaztu gabe dago\n";
-	if(erantzunOkerra2 == "") erroreak += "(*) Erantzun okerra2 zehaztu gabe dago\n";
-	if(erantzunOkerra3 == "") erroreak += "(*) Erantzun okerra3 zehaztu gabe dago\n";
-	if(arloa == "") erroreak += "(*) Gai-arloa zehaztu gabe dago\n";
-	
-	if(irudia != "" && irudia.match(".jpg") == null && irudia.match(".jpeg") == null && irudia.match(".png") == null &&
-	irudia != "" && irudia.match(".JPG") == null && irudia.match(".JPEG") == null && irudia.match(".PNG") == null)
-		erroreak += '(hautazkoa) Irudiaren formatua okerra, irudiak ".jpg", ".jpeg", ".png", ".JPG", ".JPEG" edo ".PNG" luzapena eduki behar du\n';
-	
-	if(erroreak != "") {
-		alert(erroreak);
-		return false;
-	}
-	else return true;
-});
+$(document).ready(function()
+{
+	$("#txertatu").click(function()
+	{
+		
+		var galdera = $("#galdera").val().trim(galdera).replace(/\s+/g, " ");
+		var erantzunZuzena = $("#erantzunZuzena").val();				
+		var erantzunOkerra1 = $("#erantzunOkerra1").val();
+		var erantzunOkerra2 = $("#erantzunOkerra2").val();				
+		var erantzunOkerra3 = $("#erantzunOkerra3").val();
+		var zailtasuna = $("#zailtasuna").val();				
+		var arloa = $("#arloa").val();
+		var irudia = $("#fitxategia").val();
+		var erroreak = "";
+		if(galdera == "") erroreak += "(*) Galdera ez dago ondo osatuta\n";
+		else if(galdera.length < 10) erroreak += "(*) Galderaren luzera 10ko izan behar du gutxienez\n";
+		if(erantzunZuzena == "") erroreak += "(*) Erantzun zuzena bete gabe\n";
+		if(erantzunOkerra1 == "") erroreak += "(*) Erantzun okerra1 bete gabe\n";
+		if(erantzunOkerra2 == "") erroreak += "(*) Erantzun okerra2 bete gabe\n";
+		if(erantzunOkerra3 == "") erroreak += "(*) Erantzun okerra3 bete gabe\n";
+		if(arloa == "") erroreak += "(*) Gai-arloa zehaztu gabe dago\n";
+		
+		if(irudia !== "" && irudia.match(".jpg") == null && irudia.match(".jpeg") == null && irudia.match(".png") == null &&
+		irudia !== "" && irudia.match(".JPG") == null && irudia.match(".JPEG") == null && irudia.match(".PNG") == null) 
+		{
+			erroreak += '(hautazkoa) Irudiaren formatua okerra, irudiak ".jpg", ".jpeg", ".png", ".JPG", ".JPEG" edo ".PNG" luzapena eduki behar du\n';
+		}
+		
+		if(erroreak !== "") alert(erroreak);
+		else 
+		{
+			var post_url = $("#formularioa").attr("action");
+			var request_method = $("#formularioa").attr("method");
+			var form_data = $("#formularioa").serialize();
+
+			$.ajax(
+			{
+				type: request_method,
+				url: post_url,
+				data:form_data,
+				success:function()
+				{
+					$("#AJAX").html("<br>Zure galdera datu basera gehitu da<br>");
+					galderakIkusi();
+
+				}
+
+			}
+			); 
+			return false;
+		}
+	});
 });
